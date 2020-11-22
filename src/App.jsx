a9c3afce16d099b63d58bfe49349aa13b889d43d
Emmanuel <59237112+Manii-dot@users.noreply.github.com>
2020-11-22 15:03:10 -0600

    delete unecesary files/ updating the app.jsx and Table.jsx files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tables from './components/Tables';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="p-2 bg-dark">
+        <h1 className="text-light text-center">Employee Directory</h1>
+      </header>
+      <Tables />
+
+    </div>
+  )
+}
+
---
+++ src/components/Tables.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Tables() {
+    return (
+        <div>
+            <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Avatar</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Gender</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone</th>
+    </tr>
+  </thead>
+
+            </table>
+        </div>
+    )
+}
